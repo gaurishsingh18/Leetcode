fd52b4da9b0d984257ad7d296a4a1dcd0c419cb5
gaurishsingh18 <gaurishsingh38@gmail.com>
2026-06-17 22:05:45 +0530

    Create 3898-FindtheDegreeofEachVertex

---
+++ 3898-FindtheDegreeofEachVertex/version/java/3898-FindtheDegreeofEachVertex_20260617_220541.java
@@ -0,0 +1,19 @@
+// Last updated: 17/06/2026, 22:05:41
+1class Solution {
+2    public int[] findDegrees(int[][] matrix) {
+3        int n = matrix.length;
+4        int[] ans = new int[n];
+5        
+6        // Loop through each vertex (row)
+7        for (int i = 0; i < n; i++) {
+8            int degree = 0;
+9            // Sum up the connections in the current row
+10            for (int j = 0; j < n; j++) {
+11                degree += matrix[i][j];
+12            }
+13            ans[i] = degree;
+14        }
+15        
+16        return ans;
+17    }
+18}
