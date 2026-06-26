7bf3fb4d193819ffbfcc1995c71aaf535674692f
gaurishsingh18 <gaurishsingh38@gmail.com>
2026-06-26 14:33:43 +0530

    File updated at 26/06/2026, 14:33:41

--- 2520-CounttheDigitsThatDivideaNumber/2520-CounttheDigitsThatDivideaNumber.java
+++ 2520-CounttheDigitsThatDivideaNumber/2520-CounttheDigitsThatDivideaNumber.java
@@ -1,15 +1,15 @@
-// Last updated: 26/06/2026, 14:32:21
+// Last updated: 26/06/2026, 14:33:41
 1class Solution {
-2    public int countDigits(int num) {
-3        int original = num;
-4        int count = 0;
-5        while (num > 0) {
-6            int digit = num % 10;
-7            if (original % digit == 0) {
-8                count++;
-9            }
-10            num /= 10;
-11        }
-12        return count;
-13    }
+2public int countDigits(int num) {
+3int original=num;
+4int count=0;
+5while (num>0) {
+6int digit=num%10;
+7if (original%digit==0) {
+8count++;
+9}
+10num/=10;
+11}
+12 return count;
+13}
 14}
