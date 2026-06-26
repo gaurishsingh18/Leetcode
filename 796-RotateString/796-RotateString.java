5b0a29d128621701a71e1a39f98dd7cf81d34be5
gaurishsingh18 <gaurishsingh38@gmail.com>
2026-06-26 13:42:27 +0530

    File updated at 26/06/2026, 13:42:25

--- 796-RotateString/796-RotateString.java
+++ 796-RotateString/796-RotateString.java
@@ -1,13 +1,9 @@
-// Last updated: 26/06/2026, 13:40:38
+// Last updated: 26/06/2026, 13:42:25
 1class Solution {
 2    public boolean rotateString(String s, String goal) {
-3       if(s.length() != goal.length()){
-4        return false;
-5       } 
-6       String str = s+ s;
-7       if(str.contains(goal)) {
-8        return true;
-9       }
-10       return false;
-11    }
-12}
+3        if (s.length() != goal.length()) {
+4            return false;
+5        }
+6        return (s + s).contains(goal);
+7    }
+8}
