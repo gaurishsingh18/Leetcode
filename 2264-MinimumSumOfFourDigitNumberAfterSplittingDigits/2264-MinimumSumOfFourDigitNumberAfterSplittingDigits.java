5ccff257ccbe8e685d0d94ae06786928925fba4d
gaurishsingh18 <gaurishsingh38@gmail.com>
2026-07-01 21:25:34 +0530

    Create 2264-MinimumSumOfFourDigitNumberAfterSplittingDigits

---
+++ 2264-MinimumSumOfFourDigitNumberAfterSplittingDigits/2264-MinimumSumOfFourDigitNumberAfterSplittingDigits.java
@@ -0,0 +1,14 @@
+// Last updated: 01/07/2026, 21:25:34
+class Solution {
+    public int minimumSum(int num) {
+        int[] arr = new int[4];
+        for (int i = 0; i < 4; i++) {
+            arr[i] = num % 10;
+            num /= 10;
+        }
+        Arrays.sort(arr);
+        int new1 = arr[0] * 10 + arr[2];
+        int new2 = arr[1] * 10 + arr[3];
+        return new1 + new2;
+    }
+}
