6d718ed587f2e5bf51e0b081e97dba05187ab0b0
gaurishsingh18 <gaurishsingh38@gmail.com>
2026-07-02 22:36:56 +0530

    File updated at 02/07/2026, 22:36:55

--- 3286-FindaSafeWalkThroughaGrid/3286-FindaSafeWalkThroughaGrid.java
+++ 3286-FindaSafeWalkThroughaGrid/3286-FindaSafeWalkThroughaGrid.java
@@ -1,60 +1,36 @@
-// Last updated: 02/07/2026, 21:26:24
-public record Point(int i,int j,int h){}
-
-int[][] moves = {{0,1},{0,-1},{1,0},{-1,0}};
-
-public boolean findSafeWalk(List<List<Integer>> grid, int health) {
-    int r = grid.size();
-    int c = grid.get(0).size();
-
-    Boolean[][][] memo = new Boolean[r][c][health+1];
-    boolean[][][] v = new boolean[r][c][health+1];
-
-    return go(0,0,health,new HashSet<>(),grid,new HashMap<>(),memo,v);
-}
-
-private boolean go(int i,int j,int h,HashSet<Point> v,List<List<Integer>> grid,HashMap<Point,Boolean> map,Boolean[][][] memo,boolean[][][] va)
-{
-    h = h - grid.get(i).get(j);
-    //Point p = new Point(i,j,h);
-    // if(map.containsKey(p))
-    //     return map.get(p);
-    
-    if(memo[i][j][h]!=null)
-        return memo[i][j][h];
-
-    // if(v.contains(p))
-    //     return false;
-    // //System.out.println(p+",health="+h);
-    
-    if(va[i][j][h])
-        return false;
-
-    if(h==0)
-        return false;
-    
-    if(i==grid.size()-1 && j==grid.get(0).size()-1)
-        return true;
-
-    //v.add(p);
-    va[i][j][h] = true;
-    for(int[] move:moves){
-        int nextI = i+move[0];
-        int nextJ = j+move[1];
-        if(isValid(nextI,nextJ,grid.size(),grid.get(0).size())){
-            if(go(nextI,nextJ,h,v,grid,map,memo,va))
-                return true;
-        }
-    }
-    //v.remove(p);
-    va[i][j][h] = false;
-    //map.put(p,false);
-    memo[i][j][h] = false;
-    return false;
-}
-
-private boolean isValid(int i,int j,int r,int c){
-    if(i>=0 && j>=0 && i<r && j<c)
-        return true;
-    return false;
-}
+// Last updated: 02/07/2026, 22:36:55
+1class Solution {
+2    public boolean findSafeWalk(List<List<Integer>> grid, int health) {
+3        int m = grid.size();
+4        int n = grid.get(0).size();
+5        int[][] dist = new int[m][n];
+6        for (int[] row : dist) {
+7            Arrays.fill(row, Integer.MAX_VALUE);
+8        }
+9        Deque<int[]> dq= new  ArrayDeque<>();
+10        dist[0][0] = grid.get(0).get(0);
+11        dq.offerFirst(new int[]{0, 0});
+12        int[][] dir =  {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+13        while (!dq.isEmpty()) {
+14            int[] curr = dq.pollFirst();
+15            int x= curr[0];
+16            int y =  curr[1];
+17            for (int[] d : dir) {
+18                int nx = x+d[0];
+19                int ny = y+d[1];
+20                if (nx >= 0 && ny >= 0 && nx < m && ny < n) {
+21                    int cost = grid.get(nx).get(ny);
+22                    if (dist[x][y] + cost < dist[nx][ny]) {
+23                        dist[nx][ny] = dist[x][y] + cost;
+24                        if (cost == 0) {
+25                            dq.offerFirst(new int[]{nx, ny});
+26                        } else {
+27                            dq.offerLast(new int[]{nx, ny});
+28                        }
+29                    }
+30                }
+31            }
+32        }
+33        return dist[m - 1][n - 1] < health;
+34    }
+35}
